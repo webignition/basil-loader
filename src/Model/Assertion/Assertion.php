c09a57e41c8af6c2a96922ba26e072516f8bab7c
Jon Cram <jon@webignition.net>
2019-05-20 07:47:57 +0100

    Retain assertion string in Assertion

--- src/Model/Assertion/Assertion.php
+++ src/Model/Assertion/Assertion.php
@@ -7,18 +7,26 @@ use webignition\BasilParser\Model\Value\ValueInterface;
 
 class Assertion implements AssertionInterface
 {
+    private $assertionString;
     private $identifier;
     private $comparison;
     private $value;
 
     public function __construct(
+        string $assertionString,
         IdentifierInterface $identifier,
         string $comparison,
         ?ValueInterface $value = null
     ) {
+        $this->assertionString = $assertionString;
         $this->identifier = $identifier;
         $this->comparison = $comparison;
         $this->value = $value;
+    }
+
+    public function getAssertionString(): string
+    {
+        return $this->assertionString;
     }
 
     public function getIdentifier(): IdentifierInterface
