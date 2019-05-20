6de49abe662382555ce7d587a7145f6e23e96dc3
Jon Cram <jon@webignition.net>
2019-05-20 16:55:52 +0100

    InputAction value can be null

--- src/Model/Action/InputActionInterface.php
+++ src/Model/Action/InputActionInterface.php
@@ -6,5 +6,5 @@ use webignition\BasilParser\Model\Value\ValueInterface;
 
 interface InputActionInterface extends InteractionActionInterface
 {
-    public function getValue(): ValueInterface;
+    public function getValue(): ?ValueInterface;
 }
