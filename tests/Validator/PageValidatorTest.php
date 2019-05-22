7b9faac97e58ed1d47158f05e27343b46a6d7fdd
Jon Cram <webignition@gmail.com>
2019-05-22 10:14:01 +0100

    Create PageValidator (#77)

---
+++ tests/Validator/PageValidatorTest.php
@@ -0,0 +1,46 @@
+<?php
+/** @noinspection PhpDocSignatureInspection */
+
+namespace webignition\BasilParser\Tests\Validator;
+
+use Nyholm\Psr7\Uri;
+use webignition\BasilParser\Model\Page\Page;
+use webignition\BasilParser\Model\Page\PageInterface;
+use webignition\BasilParser\Validator\PageValidator;
+
+class PageValidatorTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @var PageValidator
+     */
+    private $pageValidator;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->pageValidator = new PageValidator();
+    }
+
+    /**
+     * @dataProvider validateDataProvider
+     */
+    public function testValidate(PageInterface $page, bool $expectedIsValid)
+    {
+        $this->assertSame($expectedIsValid, $this->pageValidator->validate($page));
+    }
+
+    public function validateDataProvider(): array
+    {
+        return [
+            'empty uri is not valid' => [
+                'page' => new Page(new Uri(''), []),
+                'expectedIsValid' => false,
+            ],
+            'non-empty uri is valid' => [
+                'page' => new Page(new Uri('http://example.com/'), []),
+                'expectedIsValid' => true,
+            ],
+        ];
+    }
+}
