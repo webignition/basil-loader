7aa2427d8a22ea7cadea8f48e895a33f88f56575
Jon Cram <jon@webignition.net>
2019-05-23 12:10:20 +0100

    Create ImportCollectionFactory

--- src/Factory/Test/ImportCollectionFactory.php
+++ src/Factory/Test/ImportCollectionFactory.php
@@ -3,13 +3,14 @@
 namespace webignition\BasilParser\Factory\Test;
 
 use webignition\BasilParser\Model\Test\ImportCollection;
+use webignition\BasilParser\Model\Test\ImportCollectionInterface;
 
 class ImportCollectionFactory
 {
     const KEY_PAGES = 'config';
     const KEY_STEPS = 'imports';
 
-    public function createFromImportCollectionData(array $importCollectionData)
+    public function createFromImportCollectionData(array $importCollectionData): ImportCollectionInterface
     {
         $pageImportPaths = $importCollectionData[self::KEY_PAGES] ?? [];
         $stepImportPaths = $importCollectionData[self::KEY_STEPS] ?? [];
