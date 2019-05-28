ecf3c25df2f9f2d0a4941bd0edc43942e4450a22
Jon Cram <jon@webignition.net>
2019-05-28 10:48:54 +0100

    Update InputActionTypeFactory

--- tests/Services/ActionFactoryFactory.php
+++ tests/Services/ActionFactoryFactory.php
@@ -8,6 +8,9 @@ use webignition\BasilParser\Factory\Action\InputActionTypeFactory;
 use webignition\BasilParser\Factory\Action\InteractionActionTypeFactory;
 use webignition\BasilParser\Factory\Action\NoArgumentsActionTypeFactory;
 use webignition\BasilParser\Factory\Action\WaitActionTypeFactory;
+use webignition\BasilParser\Factory\IdentifierFactory;
+use webignition\BasilParser\Factory\ValueFactory;
+use webignition\BasilParser\IdentifierStringExtractor\IdentifierStringExtractor;
 
 class ActionFactoryFactory
 {
@@ -15,10 +18,20 @@ class ActionFactoryFactory
     {
         $actionFactory = new ActionFactory();
 
+        $identifierFactory = new IdentifierFactory();
+        $identifierStringExtractor = new IdentifierStringExtractor();
+        $valueFactory = new ValueFactory();
+
+        $inputActionTypeFactory = new InputActionTypeFactory(
+            $identifierFactory,
+            $identifierStringExtractor,
+            $valueFactory
+        );
+
         $actionFactory->addActionTypeFactory(new InteractionActionTypeFactory());
         $actionFactory->addActionTypeFactory(new WaitActionTypeFactory());
         $actionFactory->addActionTypeFactory(new NoArgumentsActionTypeFactory());
-        $actionFactory->addActionTypeFactory(new InputActionTypeFactory());
+        $actionFactory->addActionTypeFactory($inputActionTypeFactory);
 
         return $actionFactory;
     }
