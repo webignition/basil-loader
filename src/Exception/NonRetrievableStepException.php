8a9b314a0544f8d3cd7a96584dca9f1acb9ed4bc
Jon Cram <jon@webignition.net>
2019-05-28 14:06:48 +0100

    Create step provider

---
+++ src/Exception/NonRetrievableStepException.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace webignition\BasilParser\Exception;
+
+class NonRetrievableStepException extends AbstractNonRetrievableImportException
+{
+    public function __construct(string $importName, string $importPath, \Throwable $previous)
+    {
+        parent::__construct(
+            $importName,
+            $importPath,
+            'Cannot retrieve step "' . $importName . '" from "' . $importPath . '"',
+            $previous
+        );
+    }
+}
