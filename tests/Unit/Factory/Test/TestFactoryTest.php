a617e50e7c5de16520f8948208a55187a25f451d
Jon Cram <webignition@gmail.com>
2019-05-29 14:55:17 +0100

    Apply context to NonRetrievableDataProviderException (#181)

--- tests/Unit/Factory/Test/TestFactoryTest.php
+++ tests/Unit/Factory/Test/TestFactoryTest.php
@@ -6,6 +6,7 @@ namespace webignition\BasilParser\Tests\Unit\Factory\Test;
 
 use webignition\BasilParser\Exception\ContextAwareExceptionInterface;
 use webignition\BasilParser\Exception\MalformedPageElementReferenceException;
+use webignition\BasilParser\Exception\NonRetrievableDataProviderException;
 use webignition\BasilParser\Factory\StepFactory;
 use webignition\BasilParser\Factory\Test\ConfigurationFactory;
 use webignition\BasilParser\Factory\Test\TestFactory;
@@ -465,6 +466,7 @@ class TestFactoryTest extends \PHPUnit\Framework\TestCase
 
     /**
      * @dataProvider createFromTestDataThrowsMalformedPageElementReferenceExceptionDataProvider
+     * @dataProvider createFromTestDataThrowsNonRetrievableDataProviderExceptionDataProvider
      */
     public function testCreateFromTestDataThrowsException(
         string $name,
@@ -641,18 +643,80 @@ class TestFactoryTest extends \PHPUnit\Framework\TestCase
         ];
     }
 
-    public function createFromTestDataThrowsExceptionDataProvider(): array
+    public function createFromTestDataThrowsNonRetrievableDataProviderExceptionDataProvider(): array
     {
         // NonRetrievableDataProviderException
         //   thrown when trying to load a data provider that does not exist or which is not valid yaml
         //
         //   cases:
         //   - test.data references data provider that cannot be loaded
-        //   - test.data.references data provider containing invalid yaml
+        //   - test.data references data provider containing invalid yaml
         //
         //   context to be applied in:
         //   - TestFactory calling StepBuilder::build()
 
+        return [
+            'NonRetrievableDataProviderException: test.data references data provider that cannot be loaded' => [
+                'name' => 'test name',
+                'testData' => [
+                    TestFactory::KEY_CONFIGURATION => [
+                        ConfigurationFactory::KEY_BROWSER => 'chrome',
+                        ConfigurationFactory::KEY_URL => 'http://example.com',
+                    ],
+                    TestFactory::KEY_IMPORTS => [
+                        TestFactory::KEY_IMPORTS_STEPS => [
+                            'step_import_name' => FixturePathFinder::find('Step/data-parameters.yml'),
+                        ],
+                        TestFactory::KEY_IMPORTS_DATA_PROVIDERS => [
+                            'data_provider_name' => 'DataProvider/non-existent.yml'
+                        ],
+                    ],
+                    'step name' => [
+                        TestFactory::KEY_TEST_USE => 'step_import_name',
+                        TestFactory::KEY_TEST_DATA => 'data_provider_name',
+                    ],
+                ],
+                'expectedException' => NonRetrievableDataProviderException::class,
+                'expectedExceptionMessage' =>
+                    'Cannot retrieve data provider "data_provider_name" from "DataProvider/non-existent.yml"',
+                'expectedExceptionContext' =>  new ExceptionContext([
+                    ExceptionContextInterface::KEY_TEST_NAME => 'test name',
+                    ExceptionContextInterface::KEY_STEP_NAME => 'step name',
+                ])
+            ],
+            'NonRetrievableDataProviderException: test.data references data provider containing invalid yaml' => [
+                'name' => 'test name',
+                'testData' => [
+                    TestFactory::KEY_CONFIGURATION => [
+                        ConfigurationFactory::KEY_BROWSER => 'chrome',
+                        ConfigurationFactory::KEY_URL => 'http://example.com',
+                    ],
+                    TestFactory::KEY_IMPORTS => [
+                        TestFactory::KEY_IMPORTS_STEPS => [
+                            'step_import_name' => FixturePathFinder::find('Step/data-parameters.yml'),
+                        ],
+                        TestFactory::KEY_IMPORTS_DATA_PROVIDERS => [
+                            'data_provider_name' => FixturePathFinder::find('invalid-yaml.yml'),
+                        ],
+                    ],
+                    'step name' => [
+                        TestFactory::KEY_TEST_USE => 'step_import_name',
+                        TestFactory::KEY_TEST_DATA => 'data_provider_name',
+                    ],
+                ],
+                'expectedException' => NonRetrievableDataProviderException::class,
+                'expectedExceptionMessage' => 'Cannot retrieve data provider "data_provider_name" from "'
+                    . FixturePathFinder::find('invalid-yaml.yml') . '"',
+                'expectedExceptionContext' =>  new ExceptionContext([
+                    ExceptionContextInterface::KEY_TEST_NAME => 'test name',
+                    ExceptionContextInterface::KEY_STEP_NAME => 'step name',
+                ])
+            ],
+        ];
+    }
+
+    public function createFromTestDataThrowsExceptionDataProvider(): array
+    {
         // NonRetrievablePageException
         //   thrown when trying to load a page that does not exist or which is not valid yaml
         //
