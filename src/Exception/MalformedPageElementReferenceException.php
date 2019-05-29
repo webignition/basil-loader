3e4e066f78db2843f857f8cbbaa63d234d74495e
Jon Cram <webignition@gmail.com>
2019-05-29 10:37:36 +0100

    Add context to MalformedPageElementReferenceException (#164)

--- src/Exception/MalformedPageElementReferenceException.php
+++ src/Exception/MalformedPageElementReferenceException.php
@@ -2,21 +2,35 @@
 
 namespace webignition\BasilParser\Exception;
 
+use webignition\BasilParser\Model\ExceptionContext\ExceptionContext;
+use webignition\BasilParser\Model\ExceptionContext\ExceptionContextInterface;
 use webignition\BasilParser\Model\PageElementReference\PageElementReference;
 
-class MalformedPageElementReferenceException extends \Exception
+class MalformedPageElementReferenceException extends \Exception implements ContextAwareExceptionInterface
 {
     private $pageElementReference;
+    private $exceptionContext;
 
     public function __construct(PageElementReference $pageElementReference)
     {
         parent::__construct('Malformed page element reference "' . (string) $pageElementReference . '"');
 
         $this->pageElementReference = $pageElementReference;
+        $this->exceptionContext = new ExceptionContext();
     }
 
     public function getPageElementReference(): PageElementReference
     {
         return $this->pageElementReference;
+    }
+
+    public function setExceptionContext(ExceptionContextInterface $exceptionContext)
+    {
+        $this->exceptionContext = $exceptionContext;
+    }
+
+    public function getExceptionContext(): ExceptionContextInterface
+    {
+        return $this->exceptionContext;
     }
 }
