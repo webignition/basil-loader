dc6868f1ce05b3161b214ecddcd73a334b8b0dca
Jon Cram <jon@webignition.net>
2019-05-29 10:44:12 +0100

    Add context to AbstractUnknownImportException

--- src/Exception/AbstractUnknownImportException.php
+++ src/Exception/AbstractUnknownImportException.php
@@ -2,19 +2,34 @@
 
 namespace webignition\BasilParser\Exception;
 
-abstract class AbstractUnknownImportException extends \Exception
+use webignition\BasilParser\Model\ExceptionContext\ExceptionContext;
+use webignition\BasilParser\Model\ExceptionContext\ExceptionContextInterface;
+
+abstract class AbstractUnknownImportException extends \Exception implements ContextAwareExceptionInterface
 {
     private $importName;
+    private $exceptionContext;
 
     public function __construct(string $importName, string $message)
     {
         parent::__construct($message);
 
         $this->importName = $importName;
+        $this->exceptionContext = new ExceptionContext();
     }
 
     public function getImportName(): string
     {
         return $this->importName;
+    }
+
+    public function setExceptionContext(ExceptionContextInterface $exceptionContext)
+    {
+        $this->exceptionContext = $exceptionContext;
+    }
+
+    public function getExceptionContext(): ExceptionContextInterface
+    {
+        return $this->exceptionContext;
     }
 }
