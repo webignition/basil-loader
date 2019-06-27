07f4d79982318cc6eb7a739b7621f3a1ed807f17
Jon Cram <jon@webignition.net>
2019-06-27 17:07:49 +0100

    Update Test/Imports data structure

--- src/DataStructure/Test/Imports.php
+++ src/DataStructure/Test/Imports.php
@@ -3,6 +3,7 @@
 namespace webignition\BasilParser\DataStructure\Test;
 
 use webignition\BasilParser\DataStructure\AbstractDataStructure;
+use webignition\BasilParser\DataStructure\ImportList;
 
 class Imports extends AbstractDataStructure
 {
@@ -10,78 +11,31 @@ class Imports extends AbstractDataStructure
     const KEY_PAGES = 'pages';
     const KEY_DATA_PROVIDERS = 'data_providers';
 
-    const CURRENT_DIRECTORY = '.';
-    const PREVIOUS_DIRECTORY = '..';
-
-    private $basePath = '';
+    private $stepPaths;
+    private $pagePaths;
+    private $dataProviderPaths;
 
     public function __construct(array $data, string $basePath)
     {
         parent::__construct($data);
 
-        $this->basePath = $basePath;
+        $this->stepPaths = new ImportList($this->getArray(self::KEY_STEPS), $basePath);
+        $this->pagePaths = new ImportList($this->getArray(self::KEY_PAGES), $basePath);
+        $this->dataProviderPaths = new ImportList($this->getArray(self::KEY_DATA_PROVIDERS), $basePath);
     }
 
     public function getStepPaths(): array
     {
-        return $this->resolvePaths($this->getArray(self::KEY_STEPS));
+        return $this->stepPaths->getPaths();
     }
 
     public function getPagePaths(): array
     {
-        return $this->resolvePaths($this->getArray(self::KEY_PAGES));
+        return $this->pagePaths->getPaths();
     }
 
     public function getDataProviderPaths(): array
     {
-        return $this->resolvePaths($this->getArray(self::KEY_DATA_PROVIDERS));
-    }
-
-    private function resolvePaths(array $paths): array
-    {
-        if (empty($paths)) {
-            return $paths;
-        }
-
-        if (empty($this->basePath)) {
-            return $paths;
-        }
-
-        foreach ($paths as $pathIndex => $path) {
-            if ($this->isRelativePath($path)) {
-                $paths[$pathIndex] = $this->resolveRelativePath($this->basePath . $path);
-            }
-        }
-
-        return $paths;
-    }
-
-    private function isRelativePath(string $path): bool
-    {
-        $parts = explode(DIRECTORY_SEPARATOR, $path);
-
-        return count($parts) > 0 && (self::CURRENT_DIRECTORY === $parts[0] || self::PREVIOUS_DIRECTORY === $parts[0]);
-    }
-
-    private function resolveRelativePath(string $path): string
-    {
-        $resolvedPathParts = [];
-        $parts = explode(DIRECTORY_SEPARATOR, $path);
-
-        foreach ($parts as $part) {
-            $part = trim($part);
-
-            if ('' === $part || self::CURRENT_DIRECTORY === $part) {
-                continue;
-            }
-
-            if (self::PREVIOUS_DIRECTORY !== $part) {
-                array_push($resolvedPathParts, $part);
-            } elseif (count($resolvedPathParts) > 0) {
-                array_pop($resolvedPathParts);
-            }
-        }
-
-        return DIRECTORY_SEPARATOR . implode(DIRECTORY_SEPARATOR, $resolvedPathParts);
+        return $this->dataProviderPaths->getPaths();
     }
 }
