6e16e5dbb200ba6fa57cbccb2625a7b9cc8660a1
Jon Cram <jon@webignition.net>
2019-07-12 11:17:36 +0100

    Create TestBuilder

--- src/Builder/TestBuilder.php
+++ src/Builder/TestBuilder.php
@@ -2,40 +2,40 @@
 
 namespace webignition\BasilParser\Builder;
 
-use webignition\BasilModel\Step\StepInterface;
-use webignition\BasilParser\DataStructure\Step as StepData;
+use webignition\BasilModel\Test\TestInterface;
+use webignition\BasilParser\DataStructure\Test\Test as TestData;
 use webignition\BasilParser\Exception\NonRetrievablePageException;
 use webignition\BasilParser\Exception\NonRetrievableStepException;
 use webignition\BasilParser\Exception\UnknownPageElementException;
 use webignition\BasilParser\Exception\UnknownPageException;
 use webignition\BasilParser\Exception\UnknownStepException;
+use webignition\BasilParser\Factory\Test\TestFactory;
 use webignition\BasilParser\Provider\DataSet\DataSetProviderInterface;
 use webignition\BasilParser\Exception\MalformedPageElementReferenceException;
 use webignition\BasilParser\Exception\NonRetrievableDataProviderException;
 use webignition\BasilParser\Exception\UnknownDataProviderException;
-use webignition\BasilParser\Factory\StepFactory;
 use webignition\BasilParser\Provider\Page\PageProviderInterface;
 use webignition\BasilParser\Provider\Step\StepProviderInterface;
-use webignition\BasilParser\Resolver\StepResolver;
+use webignition\BasilParser\Resolver\Test\TestResolver;
 
-class StepBuilder
+class TestBuilder
 {
-    private $stepFactory;
-    private $stepResolver;
+    private $testFactory;
+    private $testResolver;
 
-    public function __construct(StepFactory $stepFactory, StepResolver $stepResolver)
+    public function __construct(TestFactory $testFactory, TestResolver $testResolver)
     {
-        $this->stepFactory = $stepFactory;
-        $this->stepResolver = $stepResolver;
+        $this->testFactory = $testFactory;
+        $this->testResolver = $testResolver;
     }
 
     /**
-     * @param StepData $stepData
+     * @param TestData $testData
+     * @param PageProviderInterface $pageProvider
      * @param StepProviderInterface $stepProvider
      * @param DataSetProviderInterface $dataSetProvider
-     * @param PageProviderInterface $pageProvider
      *
-     * @return StepInterface
+     * @return TestInterface
      *
      * @throws MalformedPageElementReferenceException
      * @throws NonRetrievableDataProviderException
@@ -47,13 +47,13 @@ class StepBuilder
      * @throws UnknownStepException
      */
     public function build(
-        StepData $stepData,
+        TestData $testData,
+        PageProviderInterface $pageProvider,
         StepProviderInterface $stepProvider,
-        DataSetProviderInterface $dataSetProvider,
-        PageProviderInterface $pageProvider
-    ): StepInterface {
-        $unresolvedStep = $this->stepFactory->createFromStepData($stepData);
+        DataSetProviderInterface $dataSetProvider
+    ): TestInterface {
+        $unresolvedTest = $this->testFactory->createFromTestData($testData->getPath(), $testData);
 
-        return $this->stepResolver->resolve($unresolvedStep, $stepProvider, $dataSetProvider, $pageProvider);
+        return $this->testResolver->resolve($unresolvedTest, $pageProvider, $stepProvider, $dataSetProvider);
     }
 }
---
+++ tests/Unit/Builder/TestBuilderTest.php
@@ -0,0 +1,249 @@
+<?php
+/** @noinspection PhpUnhandledExceptionInspection */
+/** @noinspection PhpDocSignatureInspection */
+
+namespace webignition\BasilParser\Tests\Unit\Builder;
+
+use Nyholm\Psr7\Uri;
+use webignition\BasilModel\Action\WaitAction;
+use webignition\BasilModel\Assertion\Assertion;
+use webignition\BasilModel\Assertion\AssertionComparisons;
+use webignition\BasilModel\DataSet\DataSet;
+use webignition\BasilModel\Identifier\Identifier;
+use webignition\BasilModel\Identifier\IdentifierTypes;
+use webignition\BasilModel\Page\Page;
+use webignition\BasilModel\Step\Step;
+use webignition\BasilModel\Test\Configuration;
+use webignition\BasilModel\Test\Test;
+use webignition\BasilModel\Test\TestInterface;
+use webignition\BasilModel\Value\Value;
+use webignition\BasilModel\Value\ValueTypes;
+use webignition\BasilParser\Builder\TestBuilder;
+use webignition\BasilParser\DataStructure\Step as StepData;
+use webignition\BasilParser\DataStructure\Test\Configuration as ConfigurationData;
+use webignition\BasilParser\DataStructure\Test\Imports as ImportsData;
+use webignition\BasilParser\DataStructure\Test\Test as TestData;
+use webignition\BasilParser\Provider\DataSet\DataSetProviderInterface;
+use webignition\BasilParser\Provider\DataSet\EmptyDataSetProvider;
+use webignition\BasilParser\Provider\DataSet\PopulatedDataSetProvider;
+use webignition\BasilParser\Provider\Page\EmptyPageProvider;
+use webignition\BasilParser\Provider\Page\PageProviderInterface;
+use webignition\BasilParser\Provider\Page\PopulatedPageProvider;
+use webignition\BasilParser\Provider\Step\EmptyStepProvider;
+use webignition\BasilParser\Provider\Step\PopulatedStepProvider;
+use webignition\BasilParser\Provider\Step\StepProviderInterface;
+use webignition\BasilParser\Tests\Services\PathResolverFactory;
+use webignition\BasilParser\Tests\Services\TestBuilderFactory;
+
+class TestBuilderTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @var TestBuilder
+     */
+    private $testBuilder;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->testBuilder = TestBuilderFactory::create();
+    }
+
+    /**
+     * @dataProvider buildDataProvider
+     */
+    public function testBuild(
+        TestData $testData,
+        PageProviderInterface $pageProvider,
+        StepProviderInterface $stepProvider,
+        DataSetProviderInterface $dataSetProvider,
+        TestInterface $expectedTest
+    ) {
+        $test = $this->testBuilder->build($testData, $pageProvider, $stepProvider, $dataSetProvider);
+
+        $this->assertEquals($expectedTest, $test);
+    }
+
+    public function buildDataProvider(): array
+    {
+        return [
+            'literal steps, no imports, no resolution required' => [
+                'testData' => new TestData(
+                    PathResolverFactory::create(),
+                    [
+                        TestData::KEY_CONFIGURATION => [
+                            ConfigurationData::KEY_BROWSER => 'chrome',
+                            ConfigurationData::KEY_URL => 'http://example.com/',
+                        ],
+                        'step name' => [
+                            StepData::KEY_ACTIONS => [
+                                'wait 30',
+                            ],
+                            StepData::KEY_ASSERTIONS => [
+                                '".selector" exists',
+                            ],
+                        ],
+                    ],
+                    '/path/to/test.yml'
+                ),
+                'pageProvider' => new EmptyPageProvider(),
+                'stepProvider' => new EmptyStepProvider(),
+                'dataSetProvider' => new EmptyDataSetProvider(),
+                'expectedTest' => new Test(
+                    '/path/to/test.yml',
+                    new Configuration('chrome', 'http://example.com/'),
+                    [
+                        'step name' => new Step(
+                            [
+                                new WaitAction('30'),
+                            ],
+                            [
+                                new Assertion(
+                                    '".selector" exists',
+                                    new Identifier(
+                                        IdentifierTypes::CSS_SELECTOR,
+                                        new Value(
+                                            ValueTypes::STRING,
+                                            '.selector'
+                                        )
+                                    ),
+                                    AssertionComparisons::EXISTS
+                                )
+                            ]
+                        ),
+                    ]
+                ),
+            ],
+            'imported step' => [
+                'testData' => new TestData(
+                    PathResolverFactory::create(),
+                    [
+                        TestData::KEY_CONFIGURATION => [
+                            ConfigurationData::KEY_BROWSER => 'chrome',
+                            ConfigurationData::KEY_URL => 'http://example.com/',
+                        ],
+                        TestData::KEY_IMPORTS => [
+                            ImportsData::KEY_PAGES => [
+                                'page_import_name' => '/path/to/page.yml',
+                            ],
+                            ImportsData::KEY_STEPS => [
+                                'step_import_name' => '/path/to/step.yml',
+                            ],
+                            ImportsData::KEY_DATA_PROVIDERS => [
+                                'data_provider_import_name' => '/path/to/data_set.yml',
+                            ],
+                        ],
+                        'step referencing imported page element' => [
+                            StepData::KEY_ASSERTIONS => [
+                                'page_import_name.elements.element_name exists',
+                            ],
+                        ],
+                        'step referencing imported step with imported data provider' => [
+                            StepData::KEY_USE => 'step_import_name',
+                            StepData::KEY_DATA => 'data_provider_import_name',
+                            StepData::KEY_ELEMENTS => [
+                                'element_name' => 'page_import_name.elements.element_name',
+                            ],
+                        ],
+                    ],
+                    '/path/to/test.yml'
+                ),
+                'pageProvider' => new PopulatedPageProvider([
+                    'page_import_name' => new Page(
+                        new Uri('http://example.com/'),
+                        [
+                            'element_name' => new Identifier(
+                                IdentifierTypes::CSS_SELECTOR,
+                                new Value(
+                                    ValueTypes::STRING,
+                                    '.imported-page-element-selector'
+                                )
+                            )
+                        ]
+                    )
+                ]),
+                'stepProvider' => new PopulatedStepProvider([
+                    'step_import_name' => new Step(
+                        [
+                            new WaitAction('10'),
+                        ],
+                        [
+                            new Assertion(
+                                '".imported-step-selector" exists',
+                                new Identifier(
+                                    IdentifierTypes::CSS_SELECTOR,
+                                    new Value(
+                                        ValueTypes::STRING,
+                                        '.imported-step-selector'
+                                    )
+                                ),
+                                AssertionComparisons::EXISTS
+                            )
+                        ]
+                    )
+                ]),
+                'dataSetProvider' => new PopulatedDataSetProvider([
+                    'data_provider_import_name' => [
+                        new DataSet([
+                            'foo' => 'bar',
+                        ])
+                    ],
+                ]),
+                'expectedTest' => new Test(
+                    '/path/to/test.yml',
+                    new Configuration('chrome', 'http://example.com/'),
+                    [
+                        'step referencing imported page element' => new Step(
+                            [],
+                            [
+                                new Assertion(
+                                    'page_import_name.elements.element_name exists',
+                                    new Identifier(
+                                        IdentifierTypes::CSS_SELECTOR,
+                                        new Value(
+                                            ValueTypes::STRING,
+                                            '.imported-page-element-selector'
+                                        )
+                                    ),
+                                    AssertionComparisons::EXISTS
+                                )
+                            ]
+                        ),
+                        'step referencing imported step with imported data provider' => (new Step(
+                            [
+                                new WaitAction('10'),
+                            ],
+                            [
+                                new Assertion(
+                                    '".imported-step-selector" exists',
+                                    new Identifier(
+                                        IdentifierTypes::CSS_SELECTOR,
+                                        new Value(
+                                            ValueTypes::STRING,
+                                            '.imported-step-selector'
+                                        )
+                                    ),
+                                    AssertionComparisons::EXISTS
+                                )
+                            ]
+                        ))->withDataSets([
+                            new DataSet([
+                                'foo' => 'bar',
+                            ])
+                        ])->withElementIdentifiers([
+                            'element_name' => new Identifier(
+                                IdentifierTypes::CSS_SELECTOR,
+                                new Value(
+                                    ValueTypes::STRING,
+                                    '.imported-page-element-selector'
+                                ),
+                                1,
+                                'element_name'
+                            ),
+                        ]),
+                    ]
+                ),
+            ],
+        ];
+    }
+}
