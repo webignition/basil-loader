e63b9037c51e3ae351f7567e36350a6bddd9c584
Jon Cram <jon@webignition.net>
2019-07-22 15:52:39 +0100

    Remove IdentifierStringExtractor

--- src/IdentifierStringExtractor/IdentifierStringTypeExtractorInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace webignition\BasilParser\IdentifierStringExtractor;
-
-interface IdentifierStringTypeExtractorInterface
-{
-    public function handles(string $string): bool;
-    public function extractFromStart(string $string): ?string;
-}
