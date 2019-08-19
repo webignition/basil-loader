0a9edbe112a310f71daefb4fb5d5badc7541398f
Jon Cram <jon@webignition.net>
2019-08-19 11:56:24 +0100

    Remove use of StepBuilder in StepLoader

--- src/Loader/StepLoader.php
+++ src/Loader/StepLoader.php
@@ -3,74 +3,44 @@
 namespace webignition\BasilParser\Loader;
 
 use webignition\BasilModel\Step\StepInterface;
-use webignition\BasilModelFactory\InvalidPageElementIdentifierException;
 use webignition\BasilModelFactory\MalformedPageElementReferenceException;
-use webignition\BasilParser\Builder\StepBuilder;
+use webignition\BasilModelFactory\StepFactory;
 use webignition\BasilDataStructure\Step as StepData;
-use webignition\BasilParser\Exception\CircularStepImportException;
-use webignition\BasilParser\Exception\NonRetrievableDataProviderException;
-use webignition\BasilParser\Exception\NonRetrievablePageException;
-use webignition\BasilParser\Exception\NonRetrievableStepException;
-use webignition\BasilParser\Exception\UnknownDataProviderException;
-use webignition\BasilParser\Exception\UnknownElementException;
-use webignition\BasilParser\Exception\UnknownPageElementException;
-use webignition\BasilParser\Exception\UnknownPageException;
-use webignition\BasilParser\Exception\UnknownStepException;
 use webignition\BasilParser\Exception\YamlLoaderException;
-use webignition\BasilParser\Provider\DataSet\DataSetProviderInterface;
-use webignition\BasilParser\Provider\Page\PageProviderInterface;
-use webignition\BasilParser\Provider\Step\StepProviderInterface;
 
 class StepLoader
 {
     private $yamlLoader;
-    private $stepBuilder;
+    private $stepFactory;
 
-    public function __construct(YamlLoader $yamlLoader, StepBuilder $stepBuilder)
+    public function __construct(YamlLoader $yamlLoader, StepFactory $stepFactory)
     {
         $this->yamlLoader = $yamlLoader;
-        $this->stepBuilder = $stepBuilder;
+        $this->stepFactory = $stepFactory;
     }
 
     public static function createLoader(): StepLoader
     {
         return new StepLoader(
             YamlLoader::createLoader(),
-            StepBuilder::createBuilder()
+            StepFactory::createFactory()
         );
     }
 
 
     /**
      * @param string $path
-     * @param StepProviderInterface $stepProvider
-     * @param DataSetProviderInterface $dataSetProvider
-     * @param PageProviderInterface $pageProvider
      *
      * @return StepInterface
      *
-     * @throws CircularStepImportException
-     * @throws InvalidPageElementIdentifierException
      * @throws MalformedPageElementReferenceException
-     * @throws NonRetrievableDataProviderException
-     * @throws NonRetrievablePageException
-     * @throws NonRetrievableStepException
-     * @throws UnknownDataProviderException
-     * @throws UnknownElementException
-     * @throws UnknownPageElementException
-     * @throws UnknownPageException
-     * @throws UnknownStepException
      * @throws YamlLoaderException
      */
-    public function load(
-        string $path,
-        StepProviderInterface $stepProvider,
-        DataSetProviderInterface $dataSetProvider,
-        PageProviderInterface $pageProvider
-    ): StepInterface {
+    public function load(string $path): StepInterface
+    {
         $data = $this->yamlLoader->loadArray($path);
         $stepData = new StepData($data);
 
-        return $this->stepBuilder->build($stepData, $stepProvider, $dataSetProvider, $pageProvider);
+        return $this->stepFactory->createFromStepData($stepData);
     }
 }
--- src/Provider/Step/EmptyStepProvider.php
+++ src/Provider/Step/EmptyStepProvider.php
@@ -4,27 +4,18 @@ namespace webignition\BasilParser\Provider\Step;
 
 use webignition\BasilModel\Step\StepInterface;
 use webignition\BasilParser\Exception\UnknownStepException;
-use webignition\BasilParser\Provider\DataSet\DataSetProviderInterface;
-use webignition\BasilParser\Provider\Page\PageProviderInterface;
 
 class EmptyStepProvider implements StepProviderInterface
 {
     /**
      * @param string $importName
-     * @param StepProviderInterface $stepProvider
-     * @param DataSetProviderInterface $dataSetProvider
-     * @param PageProviderInterface $pageProvider
      *
      * @return StepInterface
      *
      * @throws UnknownStepException
      */
-    public function findStep(
-        string $importName,
-        StepProviderInterface $stepProvider,
-        DataSetProviderInterface $dataSetProvider,
-        PageProviderInterface $pageProvider
-    ): StepInterface {
+    public function findStep(string $importName): StepInterface
+    {
         throw new UnknownStepException($importName);
     }
 }
