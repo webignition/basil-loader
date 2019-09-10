4530ee22328ad72381f3a15789f31f8a25b1c19f
Jon Cram <jon@webignition.net>
2019-09-10 10:59:13 +0100

    Updat page loader

--- tests/Unit/PageLoaderTest.php
+++ tests/Unit/PageLoaderTest.php
@@ -7,9 +7,11 @@ namespace webignition\BasilLoader\Tests\Unit;
 use Nyholm\Psr7\Uri;
 use webignition\BasilLoader\PageLoader;
 use webignition\BasilLoader\Tests\Services\FixturePathFinder;
+use webignition\BasilModel\Identifier\ElementIdentifierCollection;
 use webignition\BasilModel\Identifier\IdentifierCollection;
 use webignition\BasilModel\Page\Page;
 use webignition\BasilModel\Page\PageInterface;
+use webignition\BasilModel\Value\CssSelector;
 use webignition\BasilTestIdentifierFactory\TestIdentifierFactory;
 
 class PageLoaderTest extends \PHPUnit\Framework\TestCase
@@ -28,26 +30,26 @@ class PageLoaderTest extends \PHPUnit\Framework\TestCase
 
     public function loadDataProvider(): array
     {
-        $parentIdentifier = TestIdentifierFactory::createCssElementIdentifier('.form', 1, 'form');
+        $parentIdentifier = TestIdentifierFactory::createElementIdentifier(new CssSelector('.form'), 1, 'form');
 
         return [
             'empty' => [
                 'path' => FixturePathFinder::find('Empty/empty.yml'),
-                'expectedPage' => new Page(new Uri(''), new IdentifierCollection()),
+                'expectedPage' => new Page(new Uri(''), new ElementIdentifierCollection()),
             ],
             'url only' => [
                 'path' => FixturePathFinder::find('Page/example.com.url-only.yml'),
-                'expectedPage' => new Page(new Uri('https://example.com'), new IdentifierCollection()),
+                'expectedPage' => new Page(new Uri('https://example.com'), new ElementIdentifierCollection()),
             ],
             'url and element references' => [
                 'path' => FixturePathFinder::find('Page/example.com.form.yml'),
                 'expectedPage' => new Page(
                     new Uri('https://example.com'),
-                    new IdentifierCollection([
+                    new ElementIdentifierCollection([
                         'form' => $parentIdentifier,
-                        'input' => TestIdentifierFactory::createCssElementIdentifier(
-                            '.input',
-                            1,
+                        'input' => TestIdentifierFactory::createElementIdentifier(
+                            new CssSelector('.input'),
+                            null,
                             'input',
                             $parentIdentifier
                         ),
