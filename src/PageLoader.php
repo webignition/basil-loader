ee40a99f4330b2cfc70cf337a2b815ad3c74ee6e
Jon Cram <webignition@gmail.com>
2022-06-23 13:08:45 +0100

    Update PageLoader to handle page parser throwing exception if page.url is empty

--- src/PageLoader.php
+++ src/PageLoader.php
@@ -6,17 +6,20 @@ namespace webignition\BasilLoader;
 
 use webignition\BasilLoader\Exception\InvalidPageException;
 use webignition\BasilLoader\Exception\YamlLoaderException;
+use webignition\BasilLoader\Validator\InvalidResult;
 use webignition\BasilLoader\Validator\InvalidResultInterface;
 use webignition\BasilLoader\Validator\PageValidator;
+use webignition\BasilLoader\Validator\ResultType;
 use webignition\BasilModels\Model\Page\PageInterface;
+use webignition\BasilModels\Parser\Exception\InvalidPageException as InvalidPageModelException;
 use webignition\BasilModels\Parser\PageParser;
 
 class PageLoader
 {
     public function __construct(
-        private YamlLoader $yamlLoader,
-        private PageParser $pageParser,
-        private PageValidator $pageValidator
+        private readonly YamlLoader $yamlLoader,
+        private readonly PageParser $pageParser,
+        private readonly PageValidator $pageValidator
     ) {
     }
 
@@ -37,7 +40,21 @@ class PageLoader
     {
         $data = $this->yamlLoader->loadArray($path);
 
-        $page = $this->pageParser->parse($importName, $data);
+        try {
+            $page = $this->pageParser->parse($importName, $data);
+        } catch (InvalidPageModelException) {
+            $invalidResult = new InvalidResult(
+                [
+                    'import_name' => $importName,
+                    'path' => $path,
+                    'data' => $data,
+                ],
+                ResultType::PAGE,
+                PageValidator::REASON_URL_EMPTY
+            );
+
+            throw new InvalidPageException($importName, $path, $invalidResult);
+        }
 
         $validationResult = $this->pageValidator->validate($page);
         if ($validationResult instanceof InvalidResultInterface) {
